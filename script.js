0bb25ab83b5a676cc4aae0f9541c4eca45b988ee
Omer Syed <omers52195@gmail.com>
2017-05-17 12:03:16 -0700

    JS-Hands-On-Day-Two

---
+++ script.js
@@ -0,0 +1,14 @@
+var bing = "bing";
+var bong = "bong";
+
+for (var counter = 1; counter <= 100; counter++){
+  if ((counter % 3 === 0) && (counter % 5 === 0))
+    console.log(bing + " " + bong);
+  else if (counter % 3 === 0){
+    console.log(bing);
+  } else if (counter % 5 === 0){
+    console.log(bong);
+  } else {
+    console.log(counter);
+  }
+}
